ff8119176699797c097665bd6921ec611a9a21b6
Marius Becker <marius.becker.8@gmail.com>
2026-06-11 21:23:27 +0200

    Format JS

--- static/index.js
+++ static/index.js
@@ -1,17 +1,17 @@
 function sanitizeRoomName(roomName) {
     return roomName
         .toLowerCase()
-        .replaceAll(' ', '-')
-        .replaceAll(/[^a-z0-9-_]/g, '');
+        .replaceAll(" ", "-")
+        .replaceAll(/[^a-z0-9-_]/g, "");
 }
 function joinRoom(roomName) {
     roomName = sanitizeRoomName(roomName);
     window.location.href = `/room/${roomName}`;
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    const nameInput = document.getElementById('room_name_input');
-    const joinButton = document.getElementById('join_room_button');
+document.addEventListener("DOMContentLoaded", () => {
+    const nameInput = document.getElementById("room_name_input");
+    const joinButton = document.getElementById("join_room_button");
     if (
         !(nameInput instanceof HTMLInputElement) ||
         !(joinButton instanceof HTMLButtonElement)
@@ -19,17 +19,17 @@ document.addEventListener('DOMContentLoaded', () => {
         return;
     }
 
-    nameInput.addEventListener('input', (event) => {
+    nameInput.addEventListener("input", (event) => {
         nameInput.value = sanitizeRoomName(nameInput.value);
     });
 
-    nameInput.addEventListener('keypress', (event) => {
-        if (event.code === 'Enter') {
+    nameInput.addEventListener("keypress", (event) => {
+        if (event.code === "Enter") {
             joinRoom(nameInput.value);
         }
     });
 
-    joinButton.addEventListener('click', () => {
+    joinButton.addEventListener("click", () => {
         joinRoom(nameInput.value);
     });
 });
